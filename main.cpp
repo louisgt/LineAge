c6e00e0b4ac80865aab76a4fac7154a4683f6b95
louisgt <louis.gauthier@umontreal.ca>
2018-10-09 15:40:28 -0400

    implemented newick tree construction

--- main.cpp
+++ main.cpp
@@ -28,10 +28,20 @@ int main(int argc, char *argv[]) {
 
     // initialize cell array
 
-    int cellcount = std::stoi(argv[2]);
-    int fieldID = std::stoi(argv[3]);
+
+    int cellcount;
+    int fieldID;
     int cell_ctr = 1;
     int l_ctr = 0;
+
+    try{
+        //std::cout << "convert cell count and field ID" << std::endl;
+        cellcount = std::stoi(argv[2]);
+        fieldID = std::stoi(argv[3]);
+    }
+    catch(const std::invalid_argument& ia){
+        std::cout << "Invalid argument to std::stoi" << std::endl;
+    }
 
 
     //std::cout << "Considering " << cellcount << " first cells." << std::endl;
@@ -41,25 +51,43 @@ int main(int argc, char *argv[]) {
     std::ifstream infile(argv[1]);
     std::string line;
     // discard header
-    std::getline(infile, line);
+    //std::getline(infile, line);
     do
     {
         std::getline(infile, line);
         std::vector<std::string> celldata = split(line,',');  
 
+        int proID;
+        
         // check progenitor ID
-        int proID = std::stoi(celldata[4]);
+
+        try{
+            //std::cout << "convert progenitor ID" << std::endl;
+            proID = std::stoi(celldata[4]);
+            //std::cout << "progenitor is " << proID << std::endl;
+        }
+        catch(const std::invalid_argument& ia){
+            std::cout << "Invalid argument to std::stoi" << std::endl;
+        }
 
         // does it exist in the map?
         std::shared_ptr<Lineage> pLin = findLineage(LineageMap,proID);
 
         // yes: create and insert new cell in corresponding lineage
         if(pLin){
+        	//std::cout << "lineage already exists: " << pLin->getIdx() << std::endl;
         	pLin->insertCell(celldata);
         }
         // no: create new lineage and insert its progenitor cell
         else{
-        	int birthf = std::stoi(celldata[1]);
+        	int birthf;
+            try{
+                //std::cout << "convert birthframe" << std::endl;
+                birthf = std::stoi(celldata[1]);
+            }
+            catch(const std::invalid_argument& ia){
+                std::cout << "Invalid argument to std::stoi" << std::endl;
+            }
         	// insert lineage
         	l_ctr++;
 			LineageMap.emplace(proID, std::make_shared<Lineage>(birthf,l_ctr,celldata));
@@ -68,7 +96,7 @@ int main(int argc, char *argv[]) {
         cell_ctr++;
     }while(cell_ctr < cellcount);
 
-    std::cout << "FieldID\tLineage\tID\tMother\tProgenitor\tGrowthRate\tAge\tOldest\tGeneration" << std::endl;
+    std::cout << "FieldID\tLineage\tID\tFrame\tMother\tProgenitor\tGrowthRate\tAge\tOldest\tGeneration" << std::endl;
     for(auto it = LineageMap.begin(); it != LineageMap.end(); ++it)
     {
 	    int key = it->first;
@@ -76,11 +104,12 @@ int main(int argc, char *argv[]) {
 	    if(l->getSize() <= 4){
 	    	continue;
 	    }
-	    //std::cout << "#@@# Printing lineage of size " << l->getSize() << std::endl;
-	    for(auto cell_it = l->getCells()->begin(); cell_it!= l->getCells()->end(); ++cell_it)
-	    {
-	    	(*cell_it).printCell(fieldID);
-	    }
+        std::cout << ">(" << l->inOrderNewick(&(l->getCells()->front())) << ")[&&NHX:XX=1];" << std::endl;
+	    // //std::cout << "#@@# Printing lineage of size " << l->getSize() << std::endl;
+	    // for(auto cell_it = l->getCells()->begin(); cell_it!= l->getCells()->end(); ++cell_it)
+	    // {
+	    // 	(*cell_it).printCell(fieldID);
+	    // }
     }
 
     return 0;
