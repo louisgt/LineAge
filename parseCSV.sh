35317c3b9f4c828e1a082db9c92b4f46ba7c519e
louisgt <louis.gauthier@umontreal.ca>
2018-10-26 17:45:53 -0400

    added fields and the corresponding type

--- parseCSV.sh
+++ parseCSV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CSV=$1
-
-awk -F, ' FNR>1 { print sprintf("%d,%d,%.2f,%d,%d,%.5f", $1,$4,$7,$61,$101,$102); }' $CSV > csv.txt
+for f in *; do
+	awk -F, ' FNR>1 { print sprintf("%d,%d,%.2f,%.2f,%d,%d,%d,%.5f,%d,%.5f", $1,$4,$5,$7,$10,$11,$61,$98,$101,$102); }' $f > ${f%.*}.txt;
+done
